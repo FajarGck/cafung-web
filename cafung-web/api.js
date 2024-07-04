1aaceeac192db07cfa2c7be7b2e523047d1a3738
Fajar Ramadhan <111217865+FajarGck@users.noreply.github.com>
2024-07-04 11:39:06 +0700

    feature: create UI to get data from API

---
+++ cafung-web/api.js
@@ -0,0 +1,59 @@
+import {
+    showAllProducts,
+    showProductsByCategory,
+    showStores
+} from './script.js'
+
+let productsData = [];
+let drinkData = [];
+let foodData = [];
+
+async function fetchData() {
+    try {
+        const baseUrl = `http://localhost:8080`;
+        const productsResponse = await fetch(`${baseUrl}/products`);
+        const categoriesResponse = await fetch(`${baseUrl}/categories`);
+        const productDrink = await fetch(`${baseUrl}/categories/drink/products`);
+        const productFood = await fetch(`${baseUrl}/categories/food/products`);
+        const storesResponse = await fetch(`${baseUrl}/stores`);
+
+        const products = await productsResponse.json();
+        const categories = await categoriesResponse.json();
+        const stores = await storesResponse.json();
+        const drink = await productDrink.json();
+        const food = await productFood.json();
+
+        if (products.status === "ok!" && categories.status === "ok!" && stores.status === "ok!") {
+            let productsData = products;
+            let storesData = stores
+            document.getElementById('allMenu').addEventListener('click', (e) => {
+            e.preventDefault()
+            showAllProducts(productsData);
+                });
+
+                document.getElementById('menuMakanan').addEventListener('click', (e) => {
+                    e.preventDefault();
+                    showProductsByCategory(productsData, 2);
+                });
+
+                document.getElementById('menuMinuman').addEventListener('click', (e) => {
+                    e.preventDefault();
+                    showProductsByCategory(productsData, 3);
+                });
+
+                document.getElementById('btnStores').addEventListener('click', (e) => {
+                    e.preventDefault();
+                    showStores(storesData)
+                })
+                showAllProducts(productsData);
+        }
+
+    } catch (error) {
+        console.error('error fetching data: ', error)
+    }
+}
+
+
+fetchData();
+
+
---
+++ cafung-web/script.js
@@ -0,0 +1,87 @@
+const cardContainer = document.getElementById('card-container');
+
+const showAllProducts = (productsData) => {
+    const products = productsData.data.products;
+    renderProducts(products);
+}
+
+const showProductsByCategory = (productsData, categoryName) => {
+    const products = [productsData.data.products][0].filter(item => item.category.id == categoryName)
+    renderProducts(products);
+}
+const showStores = (storesData) => {
+    const stores = [storesData.data][0].stores
+    .map(item => ({
+        id: item.id,
+        name: item.name,
+        image: item.image_path,
+        owner: item.owner
+    }))
+    const uniqueStores = [];
+
+    const storeIds = new Set();
+    stores.forEach(store => {
+        if (!storeIds.has(store.id)) {
+            storeIds.add(store.id);
+            uniqueStores.push(store);
+        }
+    });
+    renderStores(uniqueStores);
+    
+    
+}
+
+const renderProducts = (products) => {
+    cardContainer.innerHTML = '';
+    products.forEach(product => {
+        let description = product.description;
+        const maxLength = 70; 
+        
+        if (description.length > maxLength) {
+            description = `${description.slice(0, maxLength)}........`;
+        }
+
+        cardContainer.innerHTML += `
+            <div class="col" id="${product.id}">
+                <div class="card">
+                    <img src="http://localhost:8080/${product.image_path}" class="card-img-top" alt="card-img">
+                    <div class="card-body">
+                        <h5 class="card-title fw-bold">${product.name}</h5>
+                        <p class="card-text fw-base">${description}</p>
+                    </div>
+                    <div class="mb-5 d-flex justify-content-around">
+                        <h3 class="price-menu fw-semibold">Rp. ${product.price}</h3>
+                        <button class="btn-beli rounded text-light">Pesan</button>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+}
+const renderStores = (storeData) => {
+    cardContainer.innerHTML = '';
+    storeData.forEach(store => {
+        cardContainer.innerHTML += `
+            <div class="col" id="${store.id}">
+                <div class="card">
+                    <img src="http://localhost:8080/${store.image}" class="card-img-top" alt="card-img">
+                    <div class="card-body">
+                        <h5 class="card-title fw-bold">${store.name}</h5>
+                        <p class="card-text fw-base">Pemilik: ${store.owner}</p>
+
+                    </div>
+                    <div class="mb-5 d-flex justify-content-around">
+                        <button class="btn-beli rounded text-light">Lihat</button>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+}
+
+export {
+    showAllProducts,
+    showProductsByCategory,
+    showStores
+    
+}
